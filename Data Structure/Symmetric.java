027573b13b630027dcae6be5f6d8064b55d45c53
Xin <zeeeengxin@users.noreply.github.com>
2017-10-23 23:11:01 -0700

    Add files via upload

---
+++ Data Structure/Symmetric.java
@@ -0,0 +1,22 @@
+package datastructure;
+
+public class Symmetric {
+	public boolean isSymmetric(TreeNode root) {
+		if (root == null)
+			return true; // don't forget to check corner case!!!
+		return helper(root.left, root.right);
+	}
+	private boolean helper(TreeNode root1, TreeNode root2) {
+		if (root1 == null && root2 == null) {
+			return true;
+		}
+		if (root1 == null || root2 == null) {
+			return false;
+		}
+		if (root1.key != root2.key) { // compare the value! not the reference...
+			return false;
+		}
+		return helper(root1.left, root2.right) &&
+				helper(root1.right, root2.left);
+	}
+}
